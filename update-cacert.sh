addabc21ecd71dfc9145bb5c2c0c5ec27ad1c99f
Remco Koopmans <me@remcokoopmans.nl>
2021-09-07 11:28:48 +0200

    Update CA chain

--- update-cacert.sh
+++ update-cacert.sh
@@ -2,7 +2,7 @@
 dir=lib/data
 
 cert=0
-curl https://curl.haxx.se/ca/cacert.pem | while read line; do
+curl https://curl.se/ca/cacert.pem | while read line; do
   if [ "-----BEGIN CERTIFICATE-----" == "$line" ]; then
       cert=1
       echo $line
